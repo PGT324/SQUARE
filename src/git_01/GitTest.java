fd507881b06861152df0d5d7f87b89d9147d301a
Guntae Park <qkrrjsxo456@naver.com>
2023-01-17 23:06:12 +0900

    git 테스트용 클래스

---
+++ src/git_01/GitTest.java
@@ -0,0 +1,12 @@
+package git_01;
+
+import java.util.Scanner;
+
+public class GitTest {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		System.out.println(num);
+	}
+
+}
